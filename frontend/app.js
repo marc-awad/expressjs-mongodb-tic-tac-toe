8aa0f89d87f88d9b1597fc7660b98f4dd6a0a5d8
Marc AWAD <marc.awad@supdevinci-edu.fr>
2025-01-27 22:41:45 +0100

    Using finding players routes

--- frontend/app.js
+++ frontend/app.js
@@ -1,9 +1,54 @@
+var currentPlayer
+
 const leaderBoardDiv = document.getElementById("leaderBoardDiv")
+const historyDiv = document.getElementById("gameHistory")
+const gameBoardDiv = document.getElementById("gameBoardDiv")
+const startButton = document.getElementById("startButton")
 
 const API_URL = "http://localhost:3001"
 const PLAYER_URL = `${API_URL}/players`
 const HISTORY_URL = `${API_URL}/history`
 const LEADERBOARD_URL = `${API_URL}/leaderboard`
+
+function hidingGameBoardAndHistory() {
+  gameBoardDiv.style.display = "none"
+  historyDiv.style.display = "none"
+}
+
+function showingGameBoardAndHistory() {
+  gameBoardDiv.style.display = "block"
+  historyDiv.style.display = "block"
+}
+
+startButton.addEventListener("click", () => {
+  const playerNameInput = document.getElementById("playerNameInput").value
+  searchPlayerFromName(playerNameInput)
+})
+
+async function searchPlayerFromName(playerName) {
+  let playerFound = false
+  const result = await fetch(PLAYER_URL)
+  const players = await result.json()
+  players.forEach((player) => {
+    if (player["name"] == playerName) {
+      currentPlayer = player
+      playerFound = true
+      console.log("Joueur trouvé :", currentPlayer)
+    }
+  })
+  if (!playerFound) {
+    let newPlayer = { name: playerName, score: 0 }
+    await fetch(PLAYER_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPlayer),
+    })
+    currentPlayer = newPlayer
+    console.log("Joueur créé :", newPlayer)
+  }
+}
 
 function createPlayerBoard(playerNumber, playerName, playerScore) {
   return `<p>${playerNumber}. ${playerName} - Score : ${playerScore}</p>`
@@ -24,3 +69,4 @@ async function fetchingLeaderBoard() {
 }
 
 fetchingLeaderBoard()
+hidingGameBoardAndHistory()
